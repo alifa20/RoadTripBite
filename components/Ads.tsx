12c70733f3ee52a3d1632324256f9b5c442727eb
ali <12068482+alifa20@users.noreply.github.com>
2024-10-01 13:06:44 +1000

    feat: admob added

---
+++ components/Ads.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from "react";
+import { Dimensions, Platform, StyleSheet, Text, View } from "react-native";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+  useForeground,
+} from "react-native-google-mobile-ads";
+
+const { height } = Dimensions.get("window");
+
+const adUnitId = __DEV__
+  ? TestIds.ADAPTIVE_BANNER
+  : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
+
+
+export const Ads = () => {
+  const bannerRef = useRef<BannerAd>(null);
+
+  // (iOS) WKWebView can terminate if app is in a "suspended state", resulting in an empty banner when app returns to foreground.
+  // Therefore it's advised to "manually" request a new ad when the app is foregrounded (https://groups.google.com/g/google-admob-ads-sdk/c/rwBpqOUr8m8).
+  useForeground(() => {
+    Platform.OS === "ios" && bannerRef.current?.load();
+  });
+
+  return (
+    <View style={styles.container}>
+       <BannerAd
+      ref={bannerRef}
+      unitId={adUnitId}
+      size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+    />
+    </View>
+  );
+};
+
+export default Ads
+const styles = StyleSheet.create({
+  container: {
+    height: height / 3,
+  },
+});
